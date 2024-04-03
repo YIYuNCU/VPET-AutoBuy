e8ef336ba913337ad443c015bc26ec5346d4dbc1
YIYuNCU <146960797+YIYuNCU@users.noreply.github.com>
2024-04-03 13:15:39 +0800

    Bug Fix
    对多语言进行适配

--- test/SettingPP.cs
+++ test/SettingPP.cs
@@ -35,17 +35,19 @@ namespace VPET.Evian.TEST
             Set.MinThirst = MW.Set["SettingPP"].GetInt("MinThirst");
             Set.MinSatiety = MW.Set["SettingPP"].GetInt("MinSatiety");
             Set.MinMood = MW.Set["SettingPP"].GetInt("MinMood");
+            Set.MinHealth = MW.Set["SettingPP"].GetInt("MinHealth");
             Set.MinDeposit = MW.Set["SettingPP"].GetInt("MinDeposit");
             Set.MinGoodThirst = MW.Set["SettingPP"].GetInt("MinGoodThirst");
             Set.MinGoodSatiety = MW.Set["SettingPP"].GetInt("MinGoodSatiety");
             Set.MinGoodMood = MW.Set["SettingPP"].GetInt("MinGoodMood");
+            Set.MinGoodHealth = MW.Set["SettingPP"].GetInt("MinGoodHealth");
             Set.Enable = MW.Set["SettingPP"].GetBool("Enable");
 
             MenuItem modset = MW.Main.ToolBar.MenuMODConfig;
             modset.Visibility = Visibility.Visible;
             var menuItem = new MenuItem()
             {
-                Header = "SettingPP",
+                Header = "SettingPP".Translate(),
                 HorizontalContentAlignment = HorizontalAlignment.Center,
             };
             menuItem.Click += (s, e) => { Setting(); };
@@ -69,6 +71,15 @@ namespace VPET.Evian.TEST
         public List<Food> Foods { get; } = new List<Food>();
         private void TakeItem(Food item)
         {
+            //通知
+            item.LoadEatTimeSource(MW);
+            item.NotifyOfPropertyChange("Description");
+
+            MW.GameSavesData.GameSave.Money -= item.Price;
+            //统计
+            MW.GameSavesData.Statistics[(gint)"stat_buytimes"]++;
+            MW.GameSavesData.Statistics[(gint)("buy_" + item.Name)]++;
+            MW.GameSavesData.Statistics[(gdbe)"stat_betterbuy"] += item.Price;
             MW.GameSavesData.GameSave.EatFood(item);
             switch (item.Type)
             {
@@ -106,9 +117,9 @@ namespace VPET.Evian.TEST
             if (Set.Enable && MW.GameSavesData.GameSave.Money >= (Set.MinDeposit > 100 ? Set.MinDeposit : 100))
             {
                 var havemoney = (Set.MaxPrice < MW.GameSavesData.GameSave.Money ? Set.MaxPrice : MW.GameSavesData.GameSave.Money);
-                List<Food> food = MW.Foods.FindAll(x => x.Price >= 2 && x.Health >= 0 && x.Exp >= 0 && x.Likability >= 0 && x.Price < havemoney //桌宠不吃负面的食物
+                List<Food> food = MW.Foods.FindAll(x => x.Price >= 2 && x.Health >= 0 && x.Exp >= 0 && x.Likability >= 0 && x.Price < havemoney && x.Feeling > 0 //桌宠不吃负面的食物
                  && !x.IsOverLoad() // 不吃超模食物
-                );
+                ) ;
 
                 if (MW.GameSavesData.GameSave.StrengthFood < sm * Set.MinSatiety * 0.01)
                 {
@@ -135,6 +146,19 @@ namespace VPET.Evian.TEST
                     MW.GameSavesData.GameSave.Money -= item.Price * 0.2;
                     TakeItem(item);
                 }
+                else if (MW.GameSavesData.GameSave.Health < 100 * Set.MinHealth * 0.01)
+                {
+                health:
+                    food = food.FindAll(x => x.Type == Food.FoodType.Drug && x.Health > (sm * Set.MinGoodHealth * 0.01 < sm ? sm * Set.MinGoodHealth * 0.01 : sm));
+                    if (food.Count == 0)
+                    {
+                        sm *= 0.5;
+                        goto health;
+                    }
+                    var item = food[Function.Rnd.Next(food.Count)];
+                    MW.GameSavesData.GameSave.Money -= item.Price * 0.2;
+                    TakeItem(item);
+                }
                 else if (MW.GameSavesData.GameSave.Feeling < smood * Set.MinMood * 0.01) 
                 {
                     if (MW.GameSavesData.GameSave.Feeling < smood * Set.MinMood * 0.01 * 0.8) 
@@ -152,7 +176,7 @@ namespace VPET.Evian.TEST
                     }
                     else
                     {
-                        mood_Snake:
+                    mood_Snake:
                         food = food.FindAll(x => x.Type == Food.FoodType.Snack && x.Feeling > (smood * Set.MinGoodMood * 0.01 < smood ? smood * Set.MinGoodMood * 0.01 : smood));
                         if (food.Count == 0)
                         {
